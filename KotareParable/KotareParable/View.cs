17621be4c1143af93e5f03885fe163e6ddb4ee61
user <user@users.noreply.github.com>
2015-05-01 10:59:28 +1200

    initial sketch of view

---
+++ KotareParable/KotareParable/View.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotareParable
+{
+    public class View
+    {
+
+        // print welcome msg  
+
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to THE KOTARE PARABLE");
+        }
+
+
+        // read input from console
+        public void GetResponse()
+        {
+            Console.ReadLine();
+        }
+        
+
+        // read from model table
+        //public void PrintOutcome(//get tablerow from controller)
+        //{
+        //    // ? new db instance
+
+        //    Console.WriteLine();  // row from table table.description
+
+        //}
+
+
+
+    }
+}
